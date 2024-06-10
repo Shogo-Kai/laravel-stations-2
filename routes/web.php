363df9a44e219d7b60e440da6a6dfa87bd8d8ad4
しょうご <155814857+Shogo-Kai@users.noreply.github.com>
2024-06-11 01:20:24 +0900

    station3

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PracticeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 // Route::get('URL', [Controllerの名前::class, 'Controller内のfunction名']);
 Route::get('/practice', [PracticeController::class, 'sample']);
