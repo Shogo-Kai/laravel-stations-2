7af26768950c051d3969ee8d2b2be33db08e81a4
しょうご <155814857+Shogo-Kai@users.noreply.github.com>
2024-06-16 02:03:19 +0900

    station6

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Movie;
+
+class MovieController extends Controller
+{
+    public function index()
+    {
+        $movies = Movie::all();
+        return view('getMovies', ['movies' => $movies]);
+    }
+}
